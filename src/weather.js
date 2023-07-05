d875e728fe607dd5d8effb597031a0bed63e82a9
Gugulethu Mdluli <gugulethumdluli98@gmail.com>
2023-07-04 19:30:00 -0600

    Added real data on web app

--- src/weather.js
+++ src/weather.js
@@ -1,1 +1,47 @@
-console.log(Hi);
+let apiKey = "1289cd044582d3271e980de23db4457d";
+let city = "Johannesburg";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+function displayDate(timestamp) {
+  let date = new Date();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return `${days[day]} ${hours}:${minutes}`;
+}
+
+function DisplayWeather(response) {
+  console.log(response.data);
+
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let TempElement = document.querySelector(".temp");
+  let dateElement = document.querySelector("#date");
+
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  TempElement.innerHTML = Math.round(response.data.main.temp);
+  dateElement.innerHTML = displayDate(response.data.dt * 1000);
+}
+axios.get(apiUrl).then(DisplayWeather);
